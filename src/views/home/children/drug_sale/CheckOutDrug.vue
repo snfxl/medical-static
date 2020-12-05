74d5d9a4de6dbd281a3b95089ef93ad3ff78655c
sn_glb <1747897227@qq.com>
2020-12-05 10:33:27 +0800

    12.5

---
+++ src/views/home/children/drug_sale/CheckOutDrug.vue
@@ -0,0 +1,22 @@
+<template>
+  <sn-three-col-space>
+    <sn-card-with-title slot="spaceContent">
+      <check-out-drug-title slot="title"/>
+    </sn-card-with-title>
+  </sn-three-col-space>
+</template>
+
+<script>
+import CheckOutDrugTitle from "@/views/home/children/drug_sale/children/CheckOutDrugTitle";
+
+export default {
+  name: "CheckOutDrug",
+  components: {
+    CheckOutDrugTitle
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
